f5eaa0385f2b313e1773fd91c08e01b18de7a090
lc <avicii950818@gmail.com>
2016-12-03 21:20:20 +0800

    表单提交链接

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 /**
  * Created by lc on 16-12-3.
  */
-$('.nav>li').hover(function () {
-    $(this).addClass('active').siblings().removeClass('active');
-},function () {
-    $(this).removeClass('active');
-});
+$.function()
+{
+    $('.nav>li').hover(function () {
+        $(this).addClass('active').siblings().removeClass('active');
+    },function () {
+        $(this).removeClass('active');
+    });
+
+
+}
+
