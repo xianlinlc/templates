8eaa00d74054f2abc41cf265d88375fddd470308
lc <avicii950818@gmail.com>
2016-12-03 21:25:36 +0800

    修改js错误

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
 /**
  * Created by lc on 16-12-3.
  */
-$.function()
-{
+
+$(function(){
     $('.nav>li').hover(function () {
         $(this).addClass('active').siblings().removeClass('active');
     },function () {
         $(this).removeClass('active');
     });
-
-
-}
+});
 
